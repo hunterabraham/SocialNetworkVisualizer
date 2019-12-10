cc9a01df1085011af3889b6f93c278f4158ca010
R-H-B <rhbell2@wisc.edu>
2019-12-10 02:36:33 -0600

    Delete DuplicateNameException.java

--- DuplicateNameException.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-/**
- * Checked exception thrown if the Name being inserted already exists in the structure.
- */
-@SuppressWarnings("serial")
-public class DuplicateNameException extends Exception {
-}
