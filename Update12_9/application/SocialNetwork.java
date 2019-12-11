667059125a0b2e72968c09aa9adc787478c9803c
R-H-B <rhbell2@wisc.edu>
2019-12-10 20:01:20 -0600

    Update SocialNetwork.java

--- Update12_9/application/SocialNetwork.java
+++ Update12_9/application/SocialNetwork.java
@@ -15,13 +15,18 @@ public class SocialNetwork implements SocialNetworkADT {
 
   @Override
   public void createNewAccount(String userName) throws DuplicateNameException {
+    if (userName == null) {
+      return;
+    }
     if (userName.equals("")) {
       throw new DuplicateNameException();
     }
+    userName = userName.replaceAll("[^a-zA-Z0-9]", "");
     if (this.string().contains(userName)) {
       throw new DuplicateNameException();
     }
-    graph.addUser(userName);
+    if (userName.contains(""))
+      graph.addUser(userName);
   }
 
   public void removeUser(String userName) throws DuplicateNameException {
@@ -46,27 +51,49 @@ public class SocialNetwork implements SocialNetworkADT {
     graph.removeFriend(user1, user2);
   }
 
+  /**
+   * Person Arraylist of all users
+   * 
+   * @return
+   */
   public ArrayList<Person> get() {
     return graph.getAllUsers();
   }
 
+  /**
+   * String Arraylist of all users
+   * 
+   * @return
+   */
   public ArrayList<String> string() {
     return graph.list();
   }
 
+  /**
+   * Search for a specific person in the graph
+   * 
+   * @param person
+   * @return
+   */
   public Person search(String person) {
     return graph.find(person);
   }
 
+  /**
+   * Clears the graph
+   */
   public void clear() {
     graph.clear();
   }
 
+  /**
+   * Find mutual friends between users
+   */
   public ArrayList<String> mutuals(String user1, String user2) {
     ArrayList<Person> one = graph.getFriendsOf(user1);
     ArrayList<Person> two = graph.getFriendsOf(user2);
     ArrayList<String> three = new ArrayList<>();
-    for (int i = 0; i < one.size(); i++ ) {
+    for (int i = 0; i < one.size(); i++) { // If both are friends add to list
       if (two.contains(one.get(i))) {
         three.add(one.get(i).getUserName());
       }
